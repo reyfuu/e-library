6ff3171557c47933c426616a91e2be775066f9ff
reyfuu <audinathanael@gmail.com>
2023-11-06 07:53:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem inventori</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="name" required>
+        <input type="text" name="pass" required>
+
+        <input type="button" value="submit">
+    </form>
+</body>
+</html>
